8c3e457f1dd6343b864bb40522e9049c1a468780
SM815 <SM815@DESKTOP-FNJ38SL>
2020-09-29 19:47:33 +0900

    car_insert()

--- Project/src/CAR/CarDAO.java
+++ Project/src/CAR/CarDAO.java
@@ -24,7 +24,7 @@ public class CarDAO {
 			// 2. connection 생성
 			conn = DriverManager.getConnection(url, db_id, db_pw);
 			if (conn != null) {
-				System.out.println("연결 성공");
+				System.out.println();
 			} else {
 				System.out.println("연결 실패");
 			}
@@ -54,33 +54,29 @@ public class CarDAO {
 
 	public void list() {
 		getConn();
-		System.out.println("====================차량목록====================");
-		System.out.println();
-		String sql = "select count(c_num) from car";
+		System.out.println("=================================차량목록=================================");
+		String sql = "select rpad(c_num,15,' ')as c_num, rpad(c_name,15,' ') as c_name, rpad(c_brand,15,' ') as c_brand, c_ivnt from car";
 		try {
 			psmt = conn.prepareStatement(sql);
-			rs= psmt.executeQuery();
-			
-			if(rs.getInt(1)>0) {
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
 				int num = rs.getInt(1);
 				String c_name = rs.getString(2);
 				String c_brand = rs.getString(3);
-				int grade = rs.getInt(4);
-				System.out.println("등록번호 : "+num+"\t 이름 : "+c_name+"\t 브랜드 :"+c_brand+"\t 등급 : "+grade);
-			}else {
-				System.out.println("차량을 등록하십시오.");
+				int ivnt = rs.getInt(4);
+				System.out.println("  등록번호 : " + num + "\t이름 : " + c_name + "\t브랜드 : " + c_brand + "\t재고량 : " + ivnt);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			close();
 		}
-		
+		System.out.println("========================================================================");
+
 	}
-	
-	
+
 	public void c_insert() {
 		getConn();
 		try {
@@ -129,50 +125,53 @@ public class CarDAO {
 		} finally {
 			close();
 		}
-		invnt();
+
 	}
 
-	public void invnt() {
-
-		boolean isOk = false;
-		String shipment = null;
-		int invnt = 0;
+	public void c_in() {
+		boolean isOk = true;
 		getConn();
-
+		int c_invnt = 0;
+		String c_name = null;
+		String c_brand = null;
 		try {
 
 			do {
+				System.out.print("추가할 차량을 입력하세요 >>>>>>>>>>>>>> ");
+				c_name = sc.next();
+				System.out.print("추가할 차량의 브랜드를 입력하세요 >>>>>>>> ");
+				c_brand = sc.next();
+				System.out.print("추가할 차량의 재고량을 입력하세요 >>>>>>>> ");
+				c_invnt = sc.nextInt();
 
-				System.out.print("출하지 : ");
-				shipment = sc.next();
+				String sql = "insert into car values(c_num_seq.nextval, ?,?,?)";
+				psmt = conn.prepareStatement(sql);
+				psmt.setString(1, c_name);
+				psmt.setString(2, c_brand);
+				psmt.setInt(3, c_invnt);
 
-				System.out.print("재고량 : ");
-				invnt = sc.nextInt();
+				int cnt = psmt.executeUpdate();
+				if (cnt > 0) {
+					System.out.println("정상적으로 입력되셨습니다.");
+				} else {
+					System.out.println("");
+				}
 
-				if (invnt == 0) {
+				if (c_invnt == 0) {
 					System.out.println("잘못입력하셨습니다.");
 					System.out.println();
 				} else {
-					isOk = true;
-					
+					System.out.println("더 추가하시겠습니까?");
+					System.out.println("[1] 예 \t [2] 아니오");
+					System.out.print(">>>>>>>>>>>>>>>>>>>>>>>");
+					int i = sc.nextInt();
+					if (i == 2) {
+						isOk = false;
+					}
+
 				}
 			} while (isOk);
 
-			String sql = "insert into grg values ((select r_num from car where c_name = ?),?,?)";
-
-			psmt = conn.prepareStatement(sql);
-			psmt.setString(1, c_name);
-			psmt.setString(2, shipment);
-			psmt.setInt(3, invnt);
-
-			int cnt = psmt.executeUpdate();
-
-			if (cnt > 0) {
-				System.out.println("재고 추가 성공");
-				
-			} else {
-				System.out.println("재고 추가 실패");
-			}
 		}
 
 		catch (Exception e) {
@@ -191,10 +190,9 @@ public class CarDAO {
 	public void c_ovr() {
 		getConn();
 		try {
-			int choice = 0;
 			System.out.println("차량을 추가 하시겠습니까?");
 			System.out.println("추가[1] 종료[2]");
-			choice = sc.nextInt();
+			int choice = sc.nextInt();
 			if (choice == 1) {
 				do {
 
@@ -205,12 +203,12 @@ public class CarDAO {
 
 					psmt = conn.prepareStatement(sql);
 					psmt.setString(1, c_name);
-					
-					rs=psmt.executeQuery();
-					
-					if(rs.getInt(1)>0) {
-					System.out.println("중복된차량 입니다.");
-					}	
+
+					rs = psmt.executeQuery();
+
+					if (rs.getInt(1) > 0) {
+						System.out.println("중복된차량 입니다.");
+					}
 				} while (choice == 1);
 				System.out.println("종료되었습니다.");
 			} else
@@ -223,8 +221,12 @@ public class CarDAO {
 		}
 	}
 
-
-
-
+	public void menu() {
+		System.out.println();
+		System.out.println("====================================MENU====================================");
+		System.out.println(" [1] 차량 목록 \t [2] 재고추가 \t [3] 새상품등록 \t [4] 주문내역\t [5] 주문취소");
+		System.out.println();
+		System.out.print(">>>>>>>>>>>>>>>>>  ");
+	}
 
 }
